cdfc3c3ff4a7cc7273834a5e57af9e800d10f5ed
utkarshverma38 <utkarsh382002@gmail.com>
2023-07-24 12:15:56 +0530

    done

---
+++ projectORS/src/main/java/com/rays/dao/TimeTableDAOInt.java
@@ -0,0 +1,12 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.TimeTableDTO;
+/**
+ * @author Utkarsh Verma 
+ *
+ */
+public interface TimeTableDAOInt extends BaseDAOInt<TimeTableDTO> {
+
+	
+}
